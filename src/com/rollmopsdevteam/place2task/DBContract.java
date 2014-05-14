9dd73f84da62045de6cd50255d06a819978ff951
Erik Türke <etuerke@googlemail.com>
2014-05-14 08:54:24 +0200

    implemented TaskListDBHelper

--- src/com/rollmopsdevteam/place2task/DBContract.java
+++ src/com/rollmopsdevteam/place2task/DBContract.java
@@ -4,11 +4,18 @@ import android.provider.BaseColumns;
 
 public final class DBContract {
 	public DBContract() {}
+
+	public static final int DATABEASE_VERSION = 1;
+	public static final String DATABASE_NAME = "TaskList.db";
+	
+	public static final String TEXT_TYPE = " TEXT";
+    public static final String COMMA_SEP = ",";
 	
 	public static abstract class TaskEntryContract implements BaseColumns {
         public static final String TABLE_NAME = "Tasks";
         public static final String COLUMN_NAME_TASK_ID = "TaskID";
         public static final String COLUMN_NAME_TASK_NAME = "TaskName";
         public static final String COLUMN_NAME_CREATION_DATE = "CreationDate";
+
 	}
 }
