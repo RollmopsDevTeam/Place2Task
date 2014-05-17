1d7aba3b5fbb5bf27659529a548bd92c50fd471a
RollmopsDevTeam <etuerke@googlemail.com>
2014-05-17 14:46:20 +0200

    some layout shifting...

--- src/com/rollmopsdevteam/place2task/LocationEditText.java
+++ src/com/rollmopsdevteam/place2task/LocationEditText.java
@@ -1,6 +1,7 @@
 package com.rollmopsdevteam.place2task;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,93 +20,108 @@ import android.widget.AutoCompleteTextView;
 import com.rollmopsdevteam.place2task.util.Constants;
 
 public class LocationEditText extends AutoCompleteTextView {
-	
+
 	private static final int _maxNumberLines = 6;
-	
+
 	private ArrayAdapter<String> _locationAdapter;
-	
+
 	private List<String> _favoriteList;
-	
+
 	public LocationEditText(Context context) {
 		super(context);
 		init(context);
 	};
-	
+
 	public LocationEditText(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init(context);
 	}
+
+	public void setFavoriteList( List<String> favoriteList ) {
+		_favoriteList = favoriteList;
+	}
 	
 	private void init(Context context) {
-		_locationAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_dropdown_item_1line);
 		
+		_favoriteList = new ArrayList<String>();
+		
+		_locationAdapter = new ArrayAdapter<String>(context,
+				android.R.layout.simple_dropdown_item_1line);
+
 		setAdapter(_locationAdapter);
 		setMaxLines(_maxNumberLines);
 		setHint(R.string.task_location_hint);
-		setInputType( InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS );
+		setInputType(InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS);
 		setThreshold(1);
-		
+
 		addTextChangedListener(new TextWatcher() {
-			
+
 			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void afterTextChanged(Editable s) {
 				// Getting user input location
-		        String location = getText().toString();
+				String location = getText().toString();
 
-		        if(location!=null && !location.equals("")){
-		        	new LocationSearchTask().execute(location);
-		        }
-				
+				if (location != null && !location.equals("")) {
+					new LocationSearchTask().execute(location);
+				}
 			}
 		});
 	}
-	
-	private class LocationSearchTask extends AsyncTask<String, Void, List<Address> > {
-		
+
+	private class LocationSearchTask extends
+			AsyncTask<String, Void, List<Address>> {
+
 		@Override
-        protected List<Address> doInBackground(String... locationName) {
-            // Creating an instance of Geocoder class
-            List<Address> addresses = null;
- 
-            try {
-                // Getting a maximum of _maxNumberLines Address that matches the input text
-                addresses = new Geocoder(getContext(), Locale.getDefault()).getFromLocationName(locationName[0], _maxNumberLines);
-            } catch (IOException e) {
-               	Log.e(Constants.LOG_TAG, "IOException: " + e.getMessage());
-            }
-            return addresses;
-        }
-		
-		 @Override
-	     protected void onPostExecute(List<Address> addresses) {
-			 if(addresses != null && addresses.size() > 0) {
-				 
-				 _locationAdapter.clear();
-				 for( Address address : addresses ) {
-					 String addressString = "";
-					 for( int line = 0; line < address.getMaxAddressLineIndex();line++) {
-						 addressString += line > 0 ? ", " + address.getAddressLine(line) : address.getAddressLine(line);
-					 }
-					 
-					 _locationAdapter.add(addressString);
-				 }
-				 _locationAdapter.getFilter().filter("");
-			 }
-		 }
-		
+		protected List<Address> doInBackground(String... locationName) {
+			List<Address> addresses = null;
+
+			try {
+				// Getting a maximum of _maxNumberLines Address that matches the
+				// input text
+				addresses = new Geocoder(getContext(), Locale.getDefault())
+						.getFromLocationName(locationName[0], _maxNumberLines);
+			} catch (IOException e) {
+				Log.e(Constants.LOG_TAG, "IOException: " + e.getMessage());
+			}
+			return addresses;
+		}
+
+		@Override
+		protected void onPostExecute(List<Address> addresses) {
+			_locationAdapter.clear();
+			_locationAdapter.addAll(_favoriteList);
+			if (addresses != null && addresses.size() > 0) {
+				for (Address address : addresses) {
+					String addressString = "";
+					for (int line = 0; line < address.getMaxAddressLineIndex(); line++) {
+						addressString += line > 0 ? ", "
+								+ address.getAddressLine(line) : address
+								.getAddressLine(line);
+					}
+					if( addressString.length() > 0 ) {	
+						_locationAdapter.add(addressString);
+					}
+				}
+				// only filter first word
+				_locationAdapter.getFilter().filter(getText().toString().split("\\s+|,|\\.")[0]);
+			}
+			
+		}
+
 	}
 
 }
